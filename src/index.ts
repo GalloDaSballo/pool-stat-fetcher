89b86b58585123fdf118b2f6b23e2b4a66ab94e2
Entreprenerd <alex@entreprenerd.xyz>
2023-05-24 13:34:50 +0200

    fix: remove dotenv for nextjs

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,10 @@
-import * as dotenv from "dotenv";
 import { providers } from "ethers";
+
 import { PoolData } from "./types";
 import balancerFetcher from "./fetchers/bal";
 import curveFetcher from "./fetchers/curve";
 import veloFetcher from "./fetchers/velo";
 
-const RPC_URLS: Record<number, string> = {
-  1: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-  5: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-  137: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-  42161: `https://arb-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-  10: `https://opt-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-  11155111: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-};
-
-dotenv.config();
 // Interfaces
 
 // ENV for RPC URL
@@ -30,13 +20,14 @@ interface ExtraInfo {
 // Balancer vault is always same addy
 
 export default async function fetchValues(
+  alchemyKey: string,
   chainId: number,
   type: string,
   address: string, // Address of Pool
   extraInfo?: ExtraInfo
 ): Promise<PoolData> {
   // Provider for given chain
-  const ALCHEMY_PROVIDER = new providers.JsonRpcProvider(RPC_URLS[chainId]);
+  const ALCHEMY_PROVIDER = new providers.JsonRpcProvider(alchemyKey);
 
   // Pass to fetcher
   if (type === "Balancer") {
@@ -54,34 +45,34 @@ export default async function fetchValues(
   // Return value
 }
 
-async function main() {
-  // const res = await fetchValues(
-  //   10,
-  //   "Balancer",
-  //   "0x7B50775383d3D6f0215A8F290f2C9e2eEBBEceb2",
-  //   {
-  //     poolId:
-  //       "0x7b50775383d3d6f0215a8f290f2c9e2eebbeceb200020000000000000000008b",
-  //   }
-  // );
+// async function main() {
+//   // const res = await fetchValues(
+//   //   10,
+//   //   "Balancer",
+//   //   "0x7B50775383d3D6f0215A8F290f2C9e2eEBBEceb2",
+//   //   {
+//   //     poolId:
+//   //       "0x7b50775383d3d6f0215a8f290f2c9e2eebbeceb200020000000000000000008b",
+//   //   }
+//   // );
 
-  // console.log("res", res);
+//   // console.log("res", res);
 
-  // const resC = await fetchValues(
-  //   10,
-  //   "Curve",
-  //   "0x1337BedC9D22ecbe766dF105c9623922A27963EC"
-  // );
+//   // const resC = await fetchValues(
+//   //   10,
+//   //   "Curve",
+//   //   "0x1337BedC9D22ecbe766dF105c9623922A27963EC"
+//   // );
 
-  // console.log("resC", resC);
+//   // console.log("resC", resC);
 
-  const resV = await fetchValues(
-    10,
-    "Velo",
-    "0x985612ff2C9409174FedcFf23d4F4761AF124F88"
-  );
+//   const resV = await fetchValues(
+//     10,
+//     "Velo",
+//     "0x985612ff2C9409174FedcFf23d4F4761AF124F88"
+//   );
 
-  console.log("resV", resV);
-}
+//   console.log("resV", resV);
+// }
 
-main();
+// main();
